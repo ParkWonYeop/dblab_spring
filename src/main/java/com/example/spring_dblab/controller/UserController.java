a264f24eacbeeb26dfe2d1aacf52662ce8138c8d
ParkWonYeop <wyp9679@gmail.com>
2023-06-25 23:40:38 +0900

    add file

--- src/main/java/com/example/spring_dblab/controller/UserController.java
+++ src/main/java/com/example/spring_dblab/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.example.spring_dblab.controller;
 
+import com.example.spring_dblab.service.UserService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+    UserService userService;
+
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public String getHello() {
+        return userService.getHello();
+    }
 }
---
+++ src/main/java/com/example/spring_dblab/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.spring_dblab.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    UserService() {
+    }
+
+    public String getHello() {
+        return "hello";
+    }
+}
